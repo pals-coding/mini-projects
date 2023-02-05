5aa5f69c9595e7acd66ae85c7803a36f65cfa5c8
Ruveyda <ruveyday20@gmail.com>
2023-02-05 21:52:39 +0300

    style: 🎨 toggle button

--- theme-toggle-button/src/App.jsx
+++ theme-toggle-button/src/App.jsx
@@ -19,30 +19,31 @@ function App() {
   });
 
   return (
-    <div style={{
-      borderRadius: "40px",
-      width: "100px",
-      backgroundColor: isToggled ? "#2a272e" : "#b2c2d9",
-      marginLeft: "100px",
-      border: isToggled ? "5px solid #2a272e" : "5px solid #b2c2d9"
-    }}>
-      <animated.div
-          style={buttonProps}
-          onClick={() => setToggle(!isToggled)}
-        >
-          <animated.img
-            src={isToggled ? "https://img.icons8.com/external-prettycons-flat-prettycons/47/null/external-moon-space-prettycons-flat-prettycons.png" : "https://img.icons8.com/external-prettycons-flat-prettycons/47/null/external-planet-earth-space-prettycons-flat-prettycons-1.png"}
-            alt="planet"
-            style={{
-              ...planetProps,
-              width: "50px",
-              height: "50px",
-              position: "absolute",
-            }}
-          />
-        </animated.div>      
+    <div
+      style={{
+        borderRadius: "40px",
+        width: "100px",
+        backgroundColor: isToggled ? "#2a272e" : "#b2c2d9",
+        border: isToggled ? "5px solid #2a272e" : "5px solid #b2c2d9",
+      }}
+    >
+      <animated.div style={buttonProps} onClick={() => setToggle(!isToggled)}>
+        <animated.img
+          src={
+            isToggled
+              ? "https://img.icons8.com/external-prettycons-flat-prettycons/47/null/external-moon-space-prettycons-flat-prettycons.png"
+              : "https://img.icons8.com/external-prettycons-flat-prettycons/47/null/external-planet-earth-space-prettycons-flat-prettycons-1.png"
+          }
+          alt="planet"
+          style={{
+            ...planetProps,
+            width: "50px",
+            height: "50px",
+            position: "absolute",
+          }}
+        />
+      </animated.div>
     </div>
-
   );
 }
 
